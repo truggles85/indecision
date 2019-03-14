f3a63bb9e3df1121c36f81c588e8b0d86bb14774
T.J <t.ruggles@live.ca>
2019-03-14 00:24:30 -0400

    Adding all the logic for app functionality

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,95 @@
-import React from "react"
+import React from 'react';
+import AddOption from '../components/AddOption';
+import Header from '../components/Header';
+import Action from '../components/Action';
+import Options from '../components/Options';
+import OptionModal from '../components/OptionModal';
 
-export default () => <div>Hello world!</div>
+export default class IndecisionApp extends React.Component {
+  state = {
+    options: [],
+    selectedOption: undefined
+  }
+  componentDidMount() {
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+  
+      if (options) {
+        this.setState(() => {
+          return {
+            options: options
+          };
+        });
+      }
+    } catch(e) {
+      //Do nothing
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+  handleDeleteOptions = () => {
+    this.setState(() => ({options: [] }));
+  }
+  handleModalReset = () => {
+    this.setState(() => ({
+      selectedOption: undefined
+    }));
+  }
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) => ({
+      options: prevState.options.filter((option) => optionToRemove !== option)
+    }));
+  }
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[randomNum];
+    this.setState(() => ({
+      selectedOption: option
+    }));
+  }
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter a valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
+    }
+    this.setState((prevState) => ({options: prevState.options.concat(option)}));
+  }
+  render() {
+    const subtitle = 'Put your life in the hands of a computer';
+
+    return (
+      <div>
+        <Header subtitle={subtitle} />
+        <div className='container'>
+          <Action
+          hasOptions={this.state.options.length > 0}
+          handlePick={this.handlePick}
+          />
+          <div className='widget'>
+            <Options 
+            options={this.state.options}
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+            />
+            <AddOption
+            handleAddOption={this.handleAddOption}
+            />
+          </div>
+        </div>
+        <OptionModal
+          selectedOption={this.state.selectedOption}
+          handleModalReset={this.handleModalReset}
+        />
+      </div>
+    );
+  };
+};
